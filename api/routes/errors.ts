8746f0d869db0b27cb1f35d73786ce755ac86108
Florian Dreschner <code@dreschner.eu>
2025-03-27 09:40:30 +0100

    fix: apply buffer to event.end rather than now

--- api/routes/errors.ts
+++ api/routes/errors.ts
@@ -4,7 +4,9 @@ import { Event } from "../models/events.ts";
 const endBuffer = 1000 * 60 * 60 * 24; // 1 day
 
 export const checkEventEnded = (event: Event) => {
-  if (event.end && event.end < new Date(Date.now() + endBuffer)) {
+  const bufferedEndTime = event.end.getTime() + endBuffer;
+
+  if (bufferedEndTime < Date.now()) {
     throw new HTTPException(403, {
       message: `Event ended on ${
         event.end.toLocaleDateString("en-US", {
