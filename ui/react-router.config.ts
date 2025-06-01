8bf941d454e25991564e7ed681a8abad9370c571
Florian Dreschner <code@dreschner.eu>
2025-06-01 20:41:23 +0200

    fix: remove flickering from index.html

--- ui/react-router.config.ts
+++ ui/react-router.config.ts
@@ -4,5 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  prerender: ["/", "/login", "/speaker"],
+  // Prerender causes flickering during hydration
+  // prerender: ["/", "/login", "/speaker"],
 } satisfies Config;
