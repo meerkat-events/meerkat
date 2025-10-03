d623063abc424f070add04e45d2d0c50e144b47b
Florian Dreschner <code@dreschner.eu>
2025-10-03 12:04:13 +0200

    chore: disable traces and use pg 17

--- db/scripts/seed.sh
+++ db/scripts/seed.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Execute the Docker command
-docker run --rm -v "$SCRIPT_DIR:/mnt" --network meerkat_default postgres:16 psql postgresql://postgres:postgres@postgres:5432/postgres -f /mnt/seed.sql
+docker run --rm -v "$SCRIPT_DIR:/mnt" --network meerkat_default postgres:17 psql postgresql://postgres:postgres@postgres:5432/postgres -f /mnt/seed.sql
